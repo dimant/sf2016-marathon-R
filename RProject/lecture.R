f908fafb5a24997dcf387b70819bc640c2c9294a
Diman Todorov <diman.todorov@outlook.com>
2016-10-27 00:39:01 -0700

    updated lecture

--- RProject/lecture.R
+++ RProject/lecture.R
@@ -97,6 +97,13 @@ ageSexEstimates[2,] <- ageSexEstimates[2,] / sum(ageSexEstimates[2,])
 
 states <- unlist(lapply(marathon$location, getState))
 
+# synthetic data
+simulateNormal <- function(d) {
+    rnorm(length(d), mean(d), sd(d))
+}
+normalGenders <- c(rep('M', sum(genders == 'M')), rep('F', sum(genders == 'F')))
+normalTimes <- c(simulateNormal(allTimes[genders == 'M']), simulateNormal(allTimes[genders == 'F']))
+
 ##################################################################################################
 
 # 1) mean
@@ -155,3 +162,69 @@ hist(ages, 10)
 hist(ages, 5)
 hist(ages, 100)
 
+# 15) box plot age ~ gender
+boxplot(allAges ~ genders, notch = TRUE)
+
+# 16) scatter plot ages ~ times
+plot(allAges ~ allTimes, xlab = "Seconds", ylab = "Age")
+
+# 17) scatter plot times ~ pace
+plot(allTimes ~ paces, xlab = "Pace", ylab = "Time")
+
+# 18)
+# one point per graph
+# https://goo.gl/25XzTn 
+
+# title
+boxplot(allAges ~ genders, notch = TRUE, main = "Distribution of ages between genders.")
+
+# xlab ylab
+plot(allTimes ~ paces, xlab = "Pace", ylab = "Time")
+
+# 19) Variance
+# https://en.wikipedia.org/wiki/Variance#Basic_properties
+
+# 20) distribution mean of ages
+curve(dnorm(x, mean(ages), var(ages) / length(ages)), from = 36.9, to = 37.1)
+
+# 21) confidence interval
+qnorm(0.025, mean(ages), var(ages) / length(ages))
+
+qnorm(1 - 0.025, mean(ages), var(ages) / length(ages))
+
+# 22) ages M vs ages F
+ks.test(
+    allTimes[genders == 'M'],
+    allTimes[genders == 'F'])
+
+# 23) age of male runners normal?
+ks.test(allTimes[genders == 'M'],
+    pnorm,
+    mean(allTimes[genders == 'M']),
+    sd(allTimes[genders == 'M']))
+
+
+# 24) Student's T-Test
+t.test(
+    normalTimes[normalGenders == 'M'],
+    normalTimes[normalGenders == 'F'])
+
+# 25) ANOVA
+fit <- lm(formula = normalTimes ~ normalGenders)
+anova(fit)
+
+# 26) fastest runner
+min(allTimes) / 3600
+# world records
+# https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=marathon%20records
+
+# 27) frequency age groups USA vs Marathon
+
+# https://www.census.gov/population/age/data/files/2012/2012gender_table1.csv
+
+barplot(as.matrix(prop.table(ageSexEstimates) * 100), beside = TRUE, legend = c("USA", "Marathon"), args.legend = list(cex = 0.75, bty = "n"), ylab = "%", xlab = "Age Group")
+
+# 28) Chi Square test ages marathon vs USA
+comparisonAgeGroups <- rbind(t(as.vector(ageSexEstimateUSA[2])), as.vector(ageSexEstimateMarathon))
+comparisonAgeGroups
+chisq.test(comparisonAgeGroups)
